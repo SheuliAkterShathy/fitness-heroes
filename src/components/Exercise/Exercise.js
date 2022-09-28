8877812a26397137a2b166386217259a2051d2d0
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-09-28 23:48:11 +0600

    exercise-info-add

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Exercise.css'
 const Exercise = ({exercise}) => {
+    const {picture,name,time}=exercise;
     return (
         <div className='exercise'>
-            <img src={exercise.picture} alt="" />
+            <img src={picture} alt="" />
+            <h3>Name:{name}</h3>
+            <p>Time required:{time}</p>
+            <button className='btn-exercise'>Add to list</button>
         </div>
     );
 };
