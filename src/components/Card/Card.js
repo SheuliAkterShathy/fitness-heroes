42e7d3eb156c7f83ca7c2ae19943d4e5486771ff
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-09-29 02:32:22 +0600

    break-time-add

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css'
 const Card = ({card}) => {
-    console.log(card)
+    
     let time=0;
     for(const item of card){
-time=time+item.time;
+    time=time+item.time;
+    }
+
+    const [breakTime,setBreakTime]=useState(0);
+
+    const addABreak=(time)=>{
+
+    setBreakTime(time)
     }
     return (
         <div>
@@ -19,16 +26,16 @@ time=time+item.time;
             <div>
                 <h4>Add a break</h4>
                 <div>
-                    <button>10s</button>
-                    <button>20s</button>
-                    <button>30s</button>
-                    <button>40s</button>
+                    <button onClick={()=>addABreak(10)}>10s</button>
+                    <button onClick={()=>addABreak(20)}>20s</button>
+                    <button onClick={()=>addABreak(30)}>30s</button>
+                    <button onClick={()=>addABreak(40)}>40</button>
                 </div>
             </div>
  
            <h4>Exercise Details</h4>
            <p>Recuired time:{time}s</p>
-           <p>Break time:</p>
+           <p>Break time:{breakTime}s</p>
         </div>
     );
 };
